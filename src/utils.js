9d98b951c9268aeba0e44185f763c66818b5b04e
Alexander Fedorov <ru9.alex@gmail.com>
2023-05-04 03:44:47 +0300

    Добавляет функции форматирования дат и времени

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,34 @@
+import dayjs from 'dayjs';
 import {escape as escapeHtml} from 'he';
+import durationPlugin from 'dayjs/plugin/duration.js';
+dayjs.extend(durationPlugin);
+
+/**
+ * @param {string} startDateTime
+ * @param {string} endDateTime
+ * @returns {string}
+ */
+function formatDuration(startDateTime, endDateTime) {
+  const milliseconds = dayjs(endDateTime).diff(startDateTime);
+
+  return dayjs.duration(milliseconds).format('  HH[h] mm[m]');
+}
+
+/**
+ * @param {string} dateTime
+ * @return {string}
+ */
+function formatDate(dateTime) {
+  return dayjs(dateTime).format('MMM D');
+}
+
+/**
+ * @param {string} dateTime
+ * @returns {string}
+ */
+function formatTime(dateTime) {
+  return dayjs(dateTime).format('HH:mm');
+}
 
 class SafeHtml extends String {}
 
@@ -25,4 +55,4 @@ function html(strings, ...values) {
   return new SafeHtml(result);
 }
 
-export {SafeHtml, html};
+export {SafeHtml, html, formatDate, formatTime, formatDuration};
